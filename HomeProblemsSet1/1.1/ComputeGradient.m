a753b851f586649e76886b5f7671fa4a8b920820
vwiklund <v.wiklund@hotmail.com>
2021-09-21 18:46:25 +0200

    finish

--- HomeProblemsSet1/1.1/ComputeGradient.m
+++
@@ -1,9 +0,0 @@
-% This function should return the gradient of f_p = f + penalty.
-% You may hard-code the gradient required for this specific problem.
-function gradF = ComputeGradient(x,mu)
-if x(1)^2+x(2)^2-1>=0
-    gradF=[2*(x(1)-1)-4*x(1)*mu*(1-x(1)^2-x(2)^2),4*(x(2)-2)-4*x(2)*mu*(1-x(1)^2-x(2)^2)];
-else
-    gradF=[2*(x(1)-1),4*(x(2)-2)];
-end
-end
