1d3d13640fa366987f51390c8c9040663e0736fc
vwiklund <v.wiklund@hotmail.com>
2021-10-09 17:07:46 +0200

    init

--- HomeProblemSet2/2.3/CalculateVelocity.m
+++ HomeProblemSet2/2.3/CalculateVelocity.m
@@ -1,9 +1,17 @@
-CalculateVelocity()
-function vel = CalculateVelocity()
-Ma = Fg-Fb-Feb
 
-a = (Fg-Fb-Feb)/M
+function v = CalculateVelocity(vel,dT,alpha,gear,bP,ENGINE_BRAKE,bT,bTMax,m)
+g = 9.82;
+Fg = m*g*sind(alpha);
+if bT < bTMax-100
+    Fb = m*g/(20)*bP;
+else
+    Fb = m*g/20*bP*exp(-(bT-(bTMax-100))/100);
+end    
+Feb = [7 5  4 3 2.5 2 1.6 1.4 1.2 1]*ENGINE_BRAKE;
 
+a = (Fg-Fb-Feb(gear))/m;
+
+v = vel+a*dT;
 
 
 end
